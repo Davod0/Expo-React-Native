081a07dbeb9d7dc5dbb04ce52c347d6da917ae57
Davod <davodnikoyii@gmail.com>
2024-09-20 14:50:06 +0200

    Fixed styling for HomeScreen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -26,11 +26,11 @@ export default function HomeScreen() {
     <ScrollView contentContainerStyle={styles.container}>
       <Text>Home Screen</Text>
       {photos.map((photo)=> (
-          <View key={photo.id} style={{width:"100%", marginBottom:20}}>
-              <Image source={{uri: photo.src.medium}} width={300} height={300} style={{width:"100%", height: 300}}/>
+          <View key={photo.id} style={styles.card}>
+              <Image source={{uri: photo.src.medium}} style={styles.image}/>
 
-              <View style={{flexDirection:"row", justifyContent:"space-between", alignItems:"center", padding:8}}>
-                <Text style={{fontSize: 20}}>{photo.photographer}</Text>
+              <View style={styles.cardFotter}>
+                <Text style={styles.fotterText}>{photo.photographer}</Text>
                 <MaterialIcons name="favorite" size={24} color={"grey"} />
               </View>
           </View>
@@ -40,7 +40,9 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-  },
+  container: {backgroundColor: '#ADD8E6'},
+  card:{width:"100%", marginBottom:20},
+  image:{width:"100%", height: 300},
+  cardFotter:{flexDirection:"row", justifyContent:"space-between", alignItems:"center", padding:8},
+  fotterText:{fontSize: 20}
 });
