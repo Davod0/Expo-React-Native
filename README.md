9fd05295c89fb1cc9f17c18fce18be2c73f63230
Davod <davodnikoyii@gmail.com>
2024-09-20 15:23:45 +0200

    The README.md file has been created

---
+++ README.md
@@ -0,0 +1,4 @@
+This is a basic mobile application built with Expo and React Native. 
+The React Navigation library is used for navigation, and I am working on nesting routes with React Navigation tabs.
+
+The goal of this project is to improve my skills in Expo, React Native, and React Navigation. I am also using an API key to fetch some photos from Pexels to display in my mobile application. The API key is hidden in the env.tsx file, and this file is not committed to the repository, so no one can see or use it on GitHub.
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,19 +2,19 @@ import { MaterialIcons } from '@expo/vector-icons'; // Se till att du importerar
 import React, { useEffect, useState } from "react";
 import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import { Photo } from "../data";
+import { API_KEY } from '../env';
 
-
+// Change name of this project
 
 export default function HomeScreen() {
 
   const [photos, setPhotos] = useState<Photo[]>([]);
-  console.log(photos);
 
   useEffect(() => {
     async function fethcer(){
 
       const url = "https://api.pexels.com/v1/search?query=nature&per_page=20";
-      const response = await fetch(url, {headers:{Authorization: "rgvwcQ2WE1CcqY6Cil80GHIKSaZmySK3P0cqgzM0Em5IbcrHxexzTDcc"}});
+      const response = await fetch(url, {headers:{Authorization: API_KEY}});
       const data = await response.json();
       setPhotos(data.photos);
     }
@@ -24,14 +24,14 @@ export default function HomeScreen() {
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Text>Home Screen</Text>
+      <Text style={styles.headerText}>Home Screen</Text>
       {photos.map((photo)=> (
           <View key={photo.id} style={styles.card}>
               <Image source={{uri: photo.src.medium}} style={styles.image}/>
 
               <View style={styles.cardFotter}>
                 <Text style={styles.fotterText}>{photo.photographer}</Text>
-                <MaterialIcons name="favorite" size={24} color={"grey"} />
+                <MaterialIcons name="favorite" size={30} color={"grey"} />
               </View>
           </View>
       ))}
@@ -40,9 +40,10 @@ export default function HomeScreen() {
 }
 
 const styles = StyleSheet.create({
-  container: {backgroundColor: '#ADD8E6'},
+  container: {backgroundColor: '#90EE90'},
+  headerText:{padding:20, fontSize:40},
   card:{width:"100%", marginBottom:20},
   image:{width:"100%", height: 300},
-  cardFotter:{flexDirection:"row", justifyContent:"space-between", alignItems:"center", padding:8},
-  fotterText:{fontSize: 20}
+  cardFotter:{flexDirection:"row", justifyContent:"space-between", alignItems:"center", padding:8, paddingBottom:20},
+  fotterText:{fontSize: 20},
 });
